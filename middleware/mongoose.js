8aa8477d2141e825d4f03b6104d5b787e80e0aaf
Fanessa <fanessah@gmail.com>
2022-11-28 16:55:45 -0500

    footer is completed

--- middleware/mongoose.js
+++ middleware/mongoose.js
@@ -4,8 +4,10 @@ const connectDb = (handler) => async (req, res) => {
   if (mongoose.connections[0].readyState) {
     return handler(req, res);
   }
-  await mongoose.connect(process.env.MONGO_URI);
+  await mongoose.connect('mongodb://127.0.0.1:27017/ecommerce-nextjs');
   return handler(req, res);
 };
+
+console.log('mongodb://127.0.0.1:27017/ecommerce-nextjs')
 
 export default connectDb;
--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,7 @@
 import Head from 'next/head';
 import Header from '../components/Header';
 import ProductList from '../components/ProductsList';
+import Footer from '../components/Footer'
 
 export default function Home() {
   return (
@@ -15,6 +16,7 @@ export default function Home() {
       </Head>
       <Header />
       <ProductList />
+      <Footer />
     </div>
   );
 }
